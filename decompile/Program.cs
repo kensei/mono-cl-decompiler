8d44ae7476540e8ca20face3cf1380f2ade029ee
kensei <kitahara-k@klab.com>
2015-04-24 06:04:46 +0900

    first import

---
+++ decompile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using ICSharpCode.Decompiler;
+using ICSharpCode.Decompiler.Ast;
+using Mono.Cecil;
+
+namespace decompile
+{
+    class MainClass
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter a file path");
+                return 1;
+            }
+
+            var filePath = args[0];
+            Console.WriteLine(filePath);
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File not Exist");
+                return 1;
+            }
+
+            var modDef = ModuleDefinition.ReadModule(args[0]);
+            foreach (var typeDef in modDef.GetTypes())
+            {
+                var builder = new AstBuilder(new DecompilerContext(modDef));
+                builder.AddType(typeDef);
+                builder.RunTransformations();
+                var output = new PlainTextOutput();
+                builder.GenerateCode(output);
+                Console.Write(output.ToString());
+            }
+
+            return 0;
+        }
+    }
+}
